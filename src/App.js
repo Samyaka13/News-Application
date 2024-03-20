b7e592d40ba37f807cf30ce01aaa2b00bdbc39f4
Samyaka13 <samyakajmera022@gmail.com>
2024-03-21 01:17:26 +0530

    Adding all the components to the webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import About from './Components/About';
+// import About from '../../textutils/src/Components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  // s = 'samyak'
+  render() { //This render compiles jsx to html and returns the html
+    return (
+      <div>
+        {/* Hello {this.s} here  */}
+        <Navbar/>
+        {/* <News/> */}
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<News/>}></Route>
+          <Route path='/about' element = {<About/>}></Route>
+        </Routes>
+        </BrowserRouter>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+ 
---
+++ src/Components/ReadMOre.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class ReadMOre extends Component {
+    constructor(){
+        super();
+    }
+  render() {
+    return (
+      <div>
+        This is read more
+      </div>
+    )
+  }
+}
+
+export default ReadMOre
